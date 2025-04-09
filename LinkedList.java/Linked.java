d0789020ce76899af2c0d0158454db08d16d962e
Abhishek <mauryaabhi20051@gmail.com>
2025-04-09 10:00:22 +0530

    Link

---
+++ LinkedList.java/Linked.java
@@ -0,0 +1,83 @@
+public class Linked {
+
+    class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data){
+        // step 1 create a node
+        Node newNode = new Node(data);
+        size++;
+        if (head==null) {
+           head=tail=newNode;
+           return; 
+        }
+        // step 2 newNodeNext=head
+        newNode.next=head;
+        // step 3
+        head=newNode;
+    }
+
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        size++;
+        if (head==null) {
+            head=tail=newNode;
+            return;
+        }
+        tail.next=newNode;
+        tail=newNode;
+    }
+
+    public void add(int idx,int data){
+        if (idx==0) {
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+        Node temp=head;
+        int i=0;
+        while (i<idx-1) {
+            temp=temp.next;
+            i++;
+        }
+        newNode.next=temp.next;
+        temp.next=newNode;
+        
+    }
+    
+    public void print(){
+        if (head==null) {
+            System.out.println("Linklist is Empty");
+            return;
+        }
+        Node temp =head;
+        while (temp!=null) {
+            System.out.print(temp.data+" -> ");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String[] args) {
+          Linked ll = new Linked();
+          ll.addFirst(2);
+          ll.addFirst(1);
+          ll.addLast(4);
+          ll.addLast(5);
+          ll.add(2, 3);
+          ll.print();
+          System.out.println(ll.size);
+    }
+}
