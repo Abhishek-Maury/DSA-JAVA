747c17daf8a6dd748d3d0cd49b63fe5cd57dea35
Abhishek <mauryaabhi20051@gmail.com>
2025-04-30 10:14:38 +0530

    new Ques

---
+++ Queue/InterleaveTwoHalves.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class InterleaveTwoHalves {
+    public static void interleaves(Queue<Integer> q){
+        int size=q.size();
+        Queue<Integer> first=new LinkedList<>();
+        for (int i = 0; i < size/2; i++) {
+            first.add(q.remove());
+        }
+
+        while (!first.isEmpty()) {
+            q.add(first.remove());
+            q.add(q.remove());
+        }
+    }
+    public static void main(String[] args) {
+        Queue<Integer> q=new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        q.add(9);
+        q.add(10);
+        interleaves(q);
+        while (!q.isEmpty()) {
+            System.out.print(q.remove()+" ");
+        }
+        System.out.println();
+
+
+    }
+}
